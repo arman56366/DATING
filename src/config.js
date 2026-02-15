b72ead8792152ecd3befa36afae940e47f45ae72
arman56366 <156896371+arman56366@users.noreply.github.com>
2026-02-15 19:53:10 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Для локальной разработки используем http://localhost:5000
 // Для Vercel используем пустую строку (используется текущий домен)
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://dating-5u6zi2zyj-arman56366s-projects.vercel.app/api' : '';
 
 export default BASE_URL;
